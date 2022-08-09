90080e1e7eb2dd1fcf1033bdc200501fe69333a3
abymani <12557637+abymani@users.noreply.github.com>
2022-08-10 00:31:00 +0500

    finally some changes in javascript and wohoo all done

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const submit = document.getElementById('submit');
 const rating_card=document.getElementById('rat_card');
 const thanks_card=document.getElementById('tha_card');
-const rate = document.getElementsByClassName('rate');
-const rating= document.getElementsByClassName('rating');
-const score=document.getElementById('score');
 
 
+
+ let score;
+ function handleRating(sc) {
+   score =sc;
+   for(let i=1;i<6;i++){
+     document.getElementById('r'+i).style.backgroundColor=" hsl(213, 19%, 18%)";
+     document.getElementById('r'+i).style.color ="hsl(217, 12%, 63%)";
+   }
+   document.getElementById('r'+sc).style.backgroundColor ="hsl(217, 12%, 63%)";
+   document.getElementById('r'+sc).style.color ="white";
+ }
 
  function handleSubmit() {
   rating_card.style.display= "none";
   thanks_card.style.display="flex";
-
+  document.getElementById('score').innerHTML ="You selected "+score+ " out of 5";
 }
